fdbbe2b22799055bc14239da67ab278f1de194c7
syahrinal-muchtar <syahrinal.muchtar@bhinneka.com>
2023-05-26 17:03:43 +0700

    Show movie image in list

--- src/components/FilmList/FilmHeader.tsx
+++ src/components/FilmList/FilmHeader.tsx
@@ -1,19 +1,24 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
-
-import { formatDate } from 'src/shared/helpers/format-date';
-import { COLOR_GRAY } from 'src/shared/styles/themes/colors';
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import CoverFilm from "src/shared/assets/images/fastX.png";
+import { formatDate } from "src/shared/helpers/format-date";
+import { COLOR_GRAY } from "src/shared/styles/themes/colors";
 
 const FilmHeaderStyle = styled.div`
   display: flex;
   flex-direction: column;
   margin-bottom: 1.7rem;
+
+  & > img {
+    margin-left: auto;
+    margin-right: auto;
+  }
 `;
 
 const FilmTitle = styled.h3`
-  font-family: 'Star Jedi', 'Open Sans', sans-serif;
+  font-family: "Star Jedi", "Open Sans", sans-serif;
   font-size: 2.2rem;
   letter-spacing: 2px;
 `;
@@ -34,6 +39,8 @@ export default function FilmHeader({
         <Link to={`/film/${id}`}>{title}</Link>
       </FilmTitle>
       <FilmDate>{formatDate(releaseDate)}</FilmDate>
+      {/* Data image not provided from SWAPI */}
+      <img src={CoverFilm} alt="Cover" width={100} />
     </FilmHeaderStyle>
   );
 }
--- src/shared/styles/base/grid.tsx
+++ src/shared/styles/base/grid.tsx
@@ -6,6 +6,7 @@ export const Container = styled.div`
   margin-right: auto;
   padding-left: 1.5rem;
   padding-right: 1.5rem;
+  margin-bottom: 10rem;
 
   /* 576px */
   @media (min-width: 36em) {
